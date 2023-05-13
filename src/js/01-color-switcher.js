778e7bc79d4d7d787b96bb16c4eba4e3e7746d9a
luftboud <ijamaharyta@gmail.com>
2023-05-13 19:12:40 +0300

    task 1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body')
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timerId = null;
 
+startButton.addEventListener('click', startColourSwitch);
+stopButton.addEventListener('click', stopColourSwitch);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startColourSwitch() {
+    timerId = setInterval(() => {
+      let color = getRandomHexColor();
+      body.style.backgroundColor = color;
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+function stopColourSwitch() {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
